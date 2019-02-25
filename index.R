b4c2894455d4c059cd8efd4475d69220c3d718c5
hrbaron <47613512+hrbaron@users.noreply.github.com>
2019-02-25 02:00:34 -0500

    Versión 6.0 Final 1 candidato

--- index.R
+++ index.R
@@ -8,9 +8,18 @@
 #install.packages("RJSONIO")
 #install.packages("leaflet")
 install.packages("streamR")
+# Paquete SVM
+#install.packages("caTools")
+#install.packages("e1071")
+#install.packages("plyr")
+#install.packages("caret")
+
 #install.packages("tm")
 #install.packages("SnowballC")
 #install.packages("DT")
+
+
+
 library(ggplot2)
 library(ggmap)
 library(shiny)
@@ -20,12 +29,20 @@ library(RJSONIO)
 library(ROAuth)
 library(leaflet)
 library(streamR)
+
+library(caTools)
+library(e1071)
+library(plyr)
+library(caret)
+
 library(tm)
 library(SnowballC)
 library(DT)
 
-ruta <-"C:/Users/hbaronfr/Desktop/CONEXION/"
-#ruta <-"D:/Universidad/Diplomado/Rstudio/"
+
+
+
+ruta <-"C:/Tweets_Candidatos/"
 setwd(ruta) 
 candidatos <-read.csv("candidatos_presidente.csv",sep = ",",
                       stringsAsFactors = F)
@@ -33,7 +50,6 @@ candidatos <-read.csv("candidatos_presidente.csv",sep = ",",
 dfCandidatos <-data.frame(candidatos)
 candidatosPresi <-unique(dfCandidatos$Nombre)
 candidatosPresi <-candidatosPresi[order(candidatosPresi,decreasing=F)]
-#setwd("C:/Users/cgordillo/OneDrive - Asesooftware S.A.S/BKASW/Documentos/Cesar/Twitter/")
 shinyApp(
   ui <- pageWithSidebar(
     #Titulo de la App
@@ -62,10 +78,11 @@ shinyApp(
       
       tabsetPanel(type = "tabs",
                   tabPanel("Mapa", h2(textOutput("encabezado")),
-                                      leafletOutput("myMap")),
+                           leafletOutput("myMap")),
                   tabPanel("Tabla", h3(textOutput("twwts")),
                            DT::dataTableOutput('table')),
-                  tabPanel("Graficas", plotOutput("histograma"))
+                  tabPanel("Grafica Bayes", plotOutput("histograma")),
+                  tabPanel("Grafica SVM", plotOutput("histogramaCaret"))
       )
       
       #Salida uno ---> Texto
@@ -109,7 +126,8 @@ shinyApp(
       
       # Create a reactive table 
       output$table <- DT::renderDataTable(
-        colnames = c('Registro','Tweet',"Clasificacion Bayes"), 
+        colnames = c('Registro','Tweet',"Clasificacion Bayes",
+                     "Clasificacion SVM"), 
         options = list(pageLength = 10),
         {
           usuario_twitter <-paste("@",
@@ -125,7 +143,7 @@ shinyApp(
           texto=json_candidatos$text
           
           #IMPRIMIR MARCADORES SEGUN EL JSON GENERADO
-          json_txt <- readLines(paste0("C:/Users/hbaronfr/Desktop/CONEXION/",
+          json_txt <- readLines(paste0("C:/Tweets_Candidatos/",
                                        "tweets_candi.json"), warn=FALSE)
           
           latitudes <- c(json_candidatos$place_lat)
@@ -156,6 +174,7 @@ shinyApp(
           # Elimina URLs
           limpia_texto = gsub("http\\w+", "", limpia_texto)
           limpia_texto = gsub("http[^[:blank:]]+", "", limpia_texto)
+          
           # Elimina a las personas
           limpia_texto = gsub("@\\w+", "", limpia_texto)
           # Elimina espacios innecesarios
@@ -165,7 +184,7 @@ shinyApp(
           limpia_texto = gsub('<.*>', '', enc2native(limpia_texto))
           # Convierte mayusculas a minusculas
           limpia_texto = tolower(limpia_texto)
-          
+          texto <- limpia_texto
           # Elimina palabras vacias (Stopwords)
           limpia_texto <- removeWords(limpia_texto, words = stopwords("spanish"))
           # Aplicacion de Stemming: Principalmente recorta las palabras a su raiz
@@ -191,8 +210,16 @@ shinyApp(
           # Seleccionamos solo la columna 4, que indica la polaridad
           polarity = clasificacion[,4]
           
-          frecuencia_sentimientos <- table(polarity)
-          parametros_barplot <-c(unique(polarity))
+          sentimientosPolar <-c(polarity)
+          sentimientosPolar <-replace(sentimientosPolar, 
+                                      sentimientosPolar=="negative",
+                                      "Negativo")
+          sentimientosPolar <-replace(sentimientosPolar, 
+                                        sentimientosPolar=="positive",
+                                        "Positivo")
+          frecuencia_sentimientos <- table(sentimientosPolar)
+          parametros_barplot <-c(unique(sentimientosPolar))
+          
           cantidadTweets <-1:length(limpia_texto)
           
           output$histograma <- renderPlot({
@@ -203,11 +230,97 @@ shinyApp(
                     names.arg=parametros_barplot, 
                     width = 0.2,
                     ylab = "Frecuencia de sentimientos",
-                    main="Sentimientos de tweets") 
+                    main="Sentimientos Bayes") 
           })
           
           
-          result = as.matrix(cbind(cantidadTweets, limpia_texto,polarity)) 
+          #-----------------------------------
+          #Clasificador Support Vector Machine
+          #-----------------------------------
+          
+          # Construye un corpus 
+          documento <- Corpus(VectorSource(limpia_texto))
+          #Convierte el texto a minuscula
+          documento <- tm_map(documento, content_transformer(tolower))
+          # Elimina palabras vacias en español
+          documento <- tm_map(documento, removeWords, stopwords("es"))
+          # Reduce las palabras a su raiz
+          documento <- tm_map(documento, stemDocument, language = "es")
+          
+          # SELECCION DE LAS CARACTERISTICAS
+          
+          #Crea la matriz de documentos
+          Frecuencia <- DocumentTermMatrix(documento)
+          # Palabras mas frecuentes en los tweets, frecuencia minima (aparicion) de 40 veces
+          findFreqTerms(Frecuencia, lowfreq = 40)
+          # Crea matriz  que elimina palabras que se mencionan poco
+          sparse <- removeSparseTerms(Frecuencia, 0.97)
+          # Retorna la matrix de sparse como un dataframe en R para trabajar el modelo SVM
+          textoSparse <- as.data.frame(as.matrix(sparse))
+          # Asigna los nombres de las palabras a las columnas del dataframe
+          colnames(textoSparse)<-make.names(colnames(textoSparse))
+          # Agrega la variable de sentimiento previamente clasificada al dataframe textoSparse
+          # con el fin de que le enseñe como clasificar los tweets y este aprenda
+          elecciones2 <-read.csv("elecciones2.csv",sep = ",",
+                                 stringsAsFactors = F)
+          #Se crea el dataFrame
+          textoSparse$sentimiento<-sample(0,nrow(textoSparse),replace=T)
+          if(length(elecciones2$sentimiento)>length(textoSparse$sentimiento))
+          {
+            for (posicion in 1:nrow(textoSparse)){
+              textoSparse$sentimiento[posicion]=elecciones2$sentimiento[posicion]
+            }  
+          }else
+          {
+            posicion <- 1
+            while(posicion <nrow(textoSparse))
+            {
+              for (rowArchivo in 1:length(elecciones2$sentimiento))
+              {
+                textoSparse$sentimiento[posicion]=elecciones2$sentimiento[rowArchivo]
+                posicion<-posicion+1
+              }
+            }
+          }
+          # establece el mismo nivel de aleatoridad para los datos en Rstudio
+          set.seed(12)
+          # # Define el 80% de las observaciones para el conjunto de datos de entrenamiento
+          split<-sample.split(textoSparse$sentimiento, SplitRatio = 0.8)# Define el 80% de las observaciones para el conjunto de datos de entrenamiento
+          # Define un conjunto de entrenamiento
+          trainSparse = subset(textoSparse, split==TRUE)
+          # Define un conjunto de evaluacion
+          testSparse = subset(textoSparse, split==FALSE)
+          
+          # Identifica como se comporta el sentimiento en el dataframe
+          table(textoSparse$sentimiento)
+          # Clasificacion con modelo SVM; Necesariamente se debe tratar la variable sentimiento como un factor
+          # trabaja sobre la base datos de entrenamiento para enseñarle al clasificador
+          SVM<-svm(as.factor(sentimiento)~ ., data=trainSparse)
+          # Muestra un resumen del clasificador para observar como esta el modelo, es decir una descripcion del modelo
+          summary(SVM)
+          # comportamiento del modelo haciendo predicciones, con los nuevos datos
+          predictSVM<-predict(SVM, newdata = textoSparse)
+          
+          sentimientosPredict <-c(predictSVM)
+          sentimientosPredict <-replace(sentimientosPredict, 
+                                        sentimientosPredict==1, "Negativo")
+          sentimientosPredict <-replace(sentimientosPredict, 
+                                        sentimientosPredict==2, "Positivo")
+          frecuencia_sentim_caret <- table(sentimientosPredict)
+          parametros_barp <-c(unique(sentimientosPredict))
+          
+          output$histogramaCaret <- renderPlot({
+            barplot(as.matrix(frecuencia_sentim_caret),
+                    beside=TRUE,
+                    horiz = F,
+                    col=c("darkblue","red"),
+                    names.arg=parametros_barp, 
+                    width = 0.2,
+                    ylab = "Frecuencia de sentimientos Caret",
+                    main="Sentimientos SVM") 
+          })
+          
+          result = as.matrix(cbind(texto,sentimientosPolar,sentimientosPredict)) 
           
           data <- result
         }
@@ -221,9 +334,9 @@ shinyApp(
         paste("Ubicacion de Tweets para ",input$candidato)
       })
     }) 
-      
     
-
+    
+    
     
   }
 )
